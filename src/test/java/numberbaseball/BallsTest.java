936cdc35fca003821dd1f38be24844bc2f9bdb92
booungyi <ansdlsgur0123@gmail.com>
2024-12-05 18:11:53 +0900

    숫자 야구게임 test 2

---
+++ src/test/java/numberbaseball/BallsTest.java
@@ -0,0 +1,12 @@
+package numberbaseball;
+
+import org.junit.jupiter.api.Test;
+
+public class BallsTest {
+    @Test
+    void name() {
+        Ball computerball = new Ball(1, 1);
+        Ball yourball = new Ball(1, 1);
+        yourball.number
+    }
+}
